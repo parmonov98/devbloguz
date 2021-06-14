56e7a208268c05814fc73e5ac2147720151c7d12
Murod <parmonov98@yandex.ru>
2021-06-14 16:59:36 +0500

    pagination centered

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -136,7 +136,7 @@ const App = (props) => {
                                     <div className="row">
                                         <Posts meta={meta} posts={posts} params={props.match.params} getPosts={getPosts} />
                                     </div>
-                                    <div className="row">
+                                    <div className="row justify-content-center">
                                         {posts.length > 0 ? <PostsPagination meta={meta} links={links} /> : "no posts"}
                                     </div>
                                 </div>
